2012373b775899b1f682fa48b24c7b7c13e1e562
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-04-15 17:15:58 -0500

    🏆 testing/contestants/ examples added to testout how fast our webserver is

---
+++ Blue-Dragon/testing/contestants/express-test.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the "index.html" file as the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
